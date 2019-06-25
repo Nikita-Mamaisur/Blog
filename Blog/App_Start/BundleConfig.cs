c5704d93107a950bfa727fafaba75efe92ca68d0
Anton Ivanchenko <iv.anton98@gmail.com>
2019-06-25 17:53:42 +0300

    BundleConfig

--- Blog/App_Start/BundleConfig.cs
+++ Blog/App_Start/BundleConfig.cs
@@ -7,14 +7,14 @@ namespace Blog
         public static void Configuration(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/content/lib")
-                .Include("~/content/bootstrap.css")
+                .Include("~/content/bootstrap.min.css")
                 .Include("~/content/summernote-bs4.css"));
 
             bundles.Add(new ScriptBundle("~/scripts/lib")
-                .Include("~/scripts/jquery-3.0.0.js")
-                .Include("~/scripts/umd/popper.js")
-                .Include("~/scripts/bootstrap.js")
-                .Include("~/scripts/summernote/summernote.js"));
+                .Include("~/scripts/jquery-3.4.1.min.js")
+                .Include("~/scripts/umd/popper.min.js")
+                .Include("~/scripts/bootstrap.min.js")
+                .Include("~/scripts/summernote/summernote-bs4.min.js"));
         }
     }
 }
